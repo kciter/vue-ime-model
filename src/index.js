d890df9ccc57ecbee654ebb82e3e96ca2abb4ca6
Lee Sun-Hyoup <kciter@naver.com>
2017-01-12 14:40:29 +0900

    Fixed lint errors

--- src/index.js
+++ src/index.js
@@ -2,46 +2,48 @@
 function plugin (Vue, options = {}) {
   const modelableTagRE = /^input|textarea?$/
 
-  function assignByPath(obj, key, value) {
-    let keys = key.split(".");
-    let last = keys.pop();
+  function assignByPath (obj, key, value) {
+    const keys = key.split('.')
+    const last = keys.pop()
     keys.forEach(key => {
-        if(!obj[key])
-            obj[key] = {};
-        obj = obj[key];
-    });
+      if (!obj[key]) {
+        obj[key] = {}
+      }
+      obj = obj[key]
+    })
 
-    obj[last] = value;
+    obj[last] = value
   }
 
-  function getByPath(obj, key) {
-    let keys = key.split(".");
-    let last = keys.pop();
+  function getByPath (obj, key) {
+    const keys = key.split('.')
+    const last = keys.pop()
     keys.forEach(key => {
-        if(!obj[key])
-            obj[key] = {};
-        obj = obj[key];
-    });
+      if (!obj[key]) {
+        obj[key] = {}
+      }
+      obj = obj[key]
+    })
 
     return obj[last]
   }
 
   Vue.directive('ime-model', {
     inserted (el, binding, vnode) {
-  	  if (!modelableTagRE.test(vnode.tag)) {
+      if (!modelableTagRE.test(vnode.tag)) {
         console.log(
           `v-ime-model is not supported on element type: <${vnode.tag}>. `,
           vnode.context
         )
       }
-  
-  	  function watchForExpression() {
-        return vnode.context.$watch(binding.expression, function() {
+
+      function watchForExpression () {
+        return vnode.context.$watch(binding.expression, () => {
           el.value = getByPath(vnode.context.$data, binding.expression)
         })
       }
-  	  var unwatch = watchForExpression()
-      el.addEventListener('input', function(e) {
+      var unwatch = watchForExpression()
+      el.addEventListener('input', (e) => {
         if (unwatch != null) {
           unwatch()
           unwatch = null
@@ -49,7 +51,7 @@ function plugin (Vue, options = {}) {
 
         assignByPath(vnode.context.$data, binding.expression, el.value)
         if (unwatch == null) {
-      	  unwatch = watchForExpression()
+          unwatch = watchForExpression()
         }
       })
       el.value = getByPath(vnode.context.$data, binding.expression)
